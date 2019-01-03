7f4715050043136f5a83cfd99f9f10e7a161fdd7
Eva van Rooden <eva.van.rooden@capgemini.com>
2019-01-03 15:47:35 +0100

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
+	include SessionsHelper # module now available to all classes inheriting from ApplicationController
 
 end
